720716a739c2aa5308dd1266af56c30f2b2212d6
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2021-11-08 06:08:39 +0200

    new

--- include/navbar.php
+++ include/navbar.php
@@ -2,11 +2,11 @@
 <nav class="navbar  navbar-expand-lg navbar-light bg-light fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand text-uppercase" >Richfield CLass</a>
-    <div class="d-flex" id="navbarNavDropdown">
+    <div class="d-flex">
       <ul class="navbar-nav">
        
         <li class="nav-item">
-          <a class="nav-link dropdown-toggle" href="" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="<?php echo BASE_URL."/index.php" ?>" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Log Out
           </a>
         </li>
